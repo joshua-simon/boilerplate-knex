411d074a9eccb4a8cf2b3cf9f2e50668d5cbfe22
Joshua-Simon <joshy.d.simon@gmail.com>
2020-01-29 17:53:59 +1100

    second attempt

--- migrations/20200129172512_instrument.js
+++ migrations/20200129172512_instrument.js
@@ -1,8 +1,12 @@
-
 exports.up = function(knex, Promise) {
+    return knex.schema.createTable('instruments', (table) => {
+      table.increments('id').primary()
+      table.string('name')
+      table.string('image')
+      table.string('blurb')
+    })
+  };
   
-};
-
-exports.down = function(knex, Promise) {
-  
-};
+  exports.down = function(knex, Promise) {
+    return knex.schema.dropTable('instruments')
+  };
