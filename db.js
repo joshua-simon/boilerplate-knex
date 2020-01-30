d40f852539b068fe5cd603b8b7652a1e2bec6045
Joshua-Simon <joshy.d.simon@gmail.com>
2020-01-31 08:17:06 +1100

    stirngs project

--- db.js
+++ db.js
@@ -2,15 +2,23 @@ const environment = process.env.NODE_ENV || 'development'
 const config = require('./knexfile')[environment]
 const connection = require('knex')(config)
 
+
+
+function getInstruments(db=connection) {
+  return db('instruments').select('image','id')
+}
+
+function getNameAndBlurb(id, db=connection) {
+  return db('instruments').where('id', id).first()
+}
+
+function editBlurb(id, updatedBlurb) {
+  return db('instruments')
+  .update({blurb:updatedBlurb}).where('id', id)
+}
+
 module.exports = {
-  getUser: getUser,
-  getUsers: getUsers
-}
-
-function getUsers (db = connection) {
-  return db('users').select()
-}
-
-function getUser (id, db = connection) {
-  return db('users').where('id', id).first()
+  getInstruments,
+  getNameAndBlurb,
+  editBlurb
 }
--- routes/users.js
+++ routes/users.js
@@ -5,11 +5,43 @@ const db = require('../db')
 const router = express.Router()
 
 
-router.get('/', (req, res) => {
-  res.send('this is a test')
+router.get('/', (req, res) => { 
+
+  db.getInstruments()
+    .then(images => {
+      console.log(images)
+      res.render('index',{images:images})    
+    })
+})
+
+router.get('/instruments/:id', (req,res) => {
+  db.getNameAndBlurb(req.params.id)
+  .then(blurb => {
+    console.log(blurb)
+    res.render('instrumentpage', {blurb: blurb})
+  })
+})
+
+router.post('/instruments/:id', (req,res) => {
+  db.editBlurb(req.params.id,req.body.blurb)
+    .then(result => {
+      console.log(result)
+      res.send('yay')
+    })
 })
 
 
+
+
+
+
+
+
+
+//this is the function to test that db.getInstruments is working
+// db.getInstruments()
+// .then(result => console.log(result))
+// })
 
 // router.get('/', (req, res) => {
 //   db.getUsers()
